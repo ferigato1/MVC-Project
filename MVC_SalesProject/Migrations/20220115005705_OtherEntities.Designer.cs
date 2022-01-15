4acc5bd7e9765bbff97cbfb047459c7c3420db51
ferigato1 <felipeferigato.fb@gmail.com>
2022-01-14 19:58:35 -0500

    Other entities and second migration

--- MVC_SalesProject/Migrations/20220115005705_OtherEntities.Designer.cs
+++ MVC_SalesProject/Migrations/20220115005705_OtherEntities.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using MVC_SalesProject.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MVC_SalesProject.Migrations
 {
     [DbContext(typeof(MVC_SalesProjectContext))]
-    partial class MVC_SalesProjectContextModelSnapshot : ModelSnapshot
+    [Migration("20220115005705_OtherEntities")]
+    partial class OtherEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- MVC_SalesProject/Models/Department.cs
+++ MVC_SalesProject/Models/Department.cs
@@ -9,6 +9,29 @@ namespace MVC_SalesProject.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        
+        //Association Departament(1) -> Seller(*)
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
 
+        public Department()
+        {
+
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initital, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initital, final));
+        }
     }
 }
